91688b60869706d84ac8cf83169be7a12b9a8b2e
William Liao <williamliao2015@gmail.com>
2024-06-02 15:21:32 +0800

    Add logging functionality to save chatbot results

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,19 @@
+import json
+
+from datetime import datetime
+
 from chains import chain
 
 
 if __name__ == "__main__":
-    print(chain.invoke({"input": "What is FOCUS?"}))
+    results = chain.invoke({
+        "messages": [
+            {"role": "user","content": "What is FOCUS in document?"}
+        ],
+        "log": []
+    })
+
+    with open(f"./logs/output {datetime.now().strftime('%Y-%m-%d %H-%M-%S')}.json", "w", encoding="utf-8") as fp:
+        json.dump(results, fp, indent=2)
+
+    print(results["messages"][-1]["content"])
