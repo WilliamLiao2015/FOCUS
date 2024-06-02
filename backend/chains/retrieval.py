65331461a4827b62f33dc98bfeaa52b7c7d4069d
William Liao <williamliao2015@gmail.com>
2024-06-02 13:25:32 +0800

    Use custom OpenAI compatible api wrapper

--- backend/chains/retrieval.py
+++ backend/chains/retrieval.py
@@ -1,6 +1,7 @@
 from langchain_core.runnables import Runnable, chain
 from openai import OpenAI
 
+from api import chat_completions
 from retriever import get_retriever
 
 
@@ -8,16 +9,9 @@ client = OpenAI(base_url="http://localhost:1234/v1", api_key="lm-studio")
 
 
 @chain
-def get_retrieval_chain(input) -> Runnable:
+def get_retrieval_chain(inputs: dict) -> Runnable:
     retriever = get_retriever()
-    context = "".join([f"{document.page_content}\n\n" for document in retriever.invoke(input["input"])])
+    prompt = inputs["input"]
+    context = "".join([f"{document.page_content}\n\n" for document in retriever.invoke(prompt)])
     system_prompt = f"Answer any use questions based solely on the context below:\n\n{context}"
-    completion = client.chat.completions.create(
-        model="lmstudio-community/Meta-Llama-3-8B-Instruct-BPE-fix-GGUF",
-        messages=[
-            {"role": "system", "content": system_prompt},
-            {"role": "user", "content": input["input"]}
-        ],
-        temperature=0.7
-    )
-    return {"input": completion.choices[0].message.content}
+    return {"input": chat_completions(prompt, system_prompt)}
